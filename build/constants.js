0532ae23501a8a0b60cfbdfea1d4a6ffa438204d
pinishahmurov <pinis@gini-apps.com>
2022-12-18 14:08:01 +0200

    add script for production, add patch for production

--- build/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SOMETHING_WENT_WRONG = exports.ROUTE_NOT_EXIST = void 0;
-exports.ROUTE_NOT_EXIST = 'Route not exist';
-exports.SOMETHING_WENT_WRONG = 'Something went wrong!';
